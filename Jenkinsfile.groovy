2c8c8e38f083849250dd5af75c446dc8ebd3e3a0
Nikita Yeryomin <nikyer@gmail.com>
2018-12-15 00:17:32 +0200

    testing config params

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -16,34 +16,34 @@ pipeline {
 		stage("Init") {
 			steps {
 				script {
+				    println('-== Building image for ' + currentEnvironment + ' environment ==-')
+                    //sleep in seconds
+                    sleep(5)
 				    configFileProvider([configFile(fileId: 'pipeline-config', variable: 'repo')]) {
-                        println('repo: ' + repo)
-                    }
 
-					println('-== Building image for ' + currentEnvironment + ' environment ==-')
-					//sleep in seconds
-					sleep(5)
-					commitHash = sh(returnStdout: true, script: "git rev-parse HEAD")
-					if ( currentEnvironment == 'QA' ) {
-						// global jenkins vars
-						sh 'env'
-						def branch = env.BRANCH_NAME
-						def environment = env.ENVIRONMENT
-						// meta vars
-						version = 1.0
-						buildExecutor = new DockerExecutor(this, [
-								repo                 : "ae/infra/jenkins/agent/linux",
-								image                : "257540276112.dkr.ecr.us-east-1.amazonaws.com/nikita-test:${commitHash}",
-								region               : "us-east-1",
-								registryAuthenticator: new AwsECRAuthenticator(this, "us-east-1"),
-								buildArgs            : [
-										BUILD_NUMBER: "${env.JOB_NAME}#${env.BUILD_NUMBER}",
-										COMMIT_HASH : commitHash
-								]
-						])
-					} else if ( currentEnvironment == 'PROD' ) {
-						def server = Artifactory.server 'artifactorymaster'
-						def rtDocker = Artifactory.docker username:'jenkinsapp2', password:'25f4s2s67Op7', server: server
+
+                        commitHash = sh(returnStdout: true, script: "git rev-parse HEAD")
+                        if ( currentEnvironment == 'QA' ) {
+                            // global jenkins vars
+                            sh 'env'
+                            def branch = env.BRANCH_NAME
+                            def environment = env.ENVIRONMENT
+                            // meta vars
+                            version = 1.0
+                            buildExecutor = new DockerExecutor(this, [
+                                    repo                 : "${repo}",
+                                    image                : "257540276112.dkr.ecr.us-east-1.amazonaws.com/nikita-test:${commitHash}",
+                                    region               : "us-east-1",
+                                    registryAuthenticator: new AwsECRAuthenticator(this, "us-east-1"),
+                                    buildArgs            : [
+                                            BUILD_NUMBER: "${env.JOB_NAME}#${env.BUILD_NUMBER}",
+                                            COMMIT_HASH : commitHash
+                                    ]
+                            ])
+                        } else if ( currentEnvironment == 'PROD' ) {
+                            def server = Artifactory.server 'artifactorymaster'
+                            def rtDocker = Artifactory.docker username:'jenkinsapp2', password:'25f4s2s67Op7', server: server
+                        }
 					}
 				}
 			}
